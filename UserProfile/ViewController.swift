2518194f93de23ff18918135656f1b86eccb2a93
Jake <jake@itovi.com>
2018-12-04 16:33:03 -0700

    Initial Commit

---
+++ UserProfile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserProfile
+//
+//  Created by Jake Swedenburg on 12/4/18.
+//  Copyright © 2018 Jake Swedenburg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ UserProfileTests/UserProfileTests.swift
@@ -0,0 +1,34 @@
+//
+//  UserProfileTests.swift
+//  UserProfileTests
+//
+//  Created by Jake Swedenburg on 12/4/18.
+//  Copyright © 2018 Jake Swedenburg. All rights reserved.
+//
+
+import XCTest
+@testable import UserProfile
+
+class UserProfileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
